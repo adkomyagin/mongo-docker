b85ba242d75e23dae501cb31b6d088b1cc833930
Alex Komyagin <alex.komyagin@10gen.com>
2014-11-28 10:38:14 -0500

    Update tester_S16237.py

--- tester_S16237.py
+++ tester_S16237.py
@@ -7,10 +7,10 @@ from timeit import default_timer as timer
 from pymongo import ReadPreference
 import time
 
-#create an instance of MDocker
+# create an instance of MDocker
 docker = MDocker()
 
-# create new system (image, params, sleep time)
+# create new system (image, params, mongo port)
 deploy = collections.OrderedDict()
 default_image = "alex/mongodb_1"
 deploy["mongo_D1"] = (default_image, "mongod --smallfiles --replSet xxx", 27017)
@@ -18,6 +18,7 @@ deploy["mongo_D2"] = (default_image, "mongod --smallfiles --replSet xxx", 27017)
 deploy["mongo_CFG"] = (default_image, "mongod --smallfiles", 27017)
 deploy["mongo_S1"] = (default_image, "mongos --configdb mongo_CFG:27017", 27017)
 
+# deploy the system
 res = docker.deploy(deploy)
 if res != 1:
 	print("Failed deploying. Aborting")
@@ -37,6 +38,7 @@ print("Init replica set..")
 client = MongoClient('mongo_D1', 27017)
 client.admin.command('replSetInitiate', replSetConfig)
 
+# we can use this fancy function to wait until the host becomes primary
 wait_to_become_primary('mongo_D1', 27017)
 
 print("Sharding the collection..")
@@ -47,6 +49,7 @@ client.admin.command('shardCollection', 'test.test', key={'_id': 1})
 
 client.admin.command('setParameter', 1, logLevel=5)
 
+# TEST TIME!
 print("Napping just in case...")
 time.sleep(5)
 
@@ -60,6 +63,7 @@ for x in range(0, 5):
 	print("Time: " + str(ts))
 	time.sleep(6)
 
+# Simulating the primary becoming a TCP black hole
 docker.block("mongo_D1")
 print("Napping 30 sec")
 time.sleep(30)
